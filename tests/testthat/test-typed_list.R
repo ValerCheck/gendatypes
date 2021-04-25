3bbeb88354546250c8e91cf45503b947fc4dcc4d
ValerCheck <mypowerismind@gmail.com>
2021-04-25 20:45:07 +0300

    Increase test coverage of typed_list

--- tests/testthat/test-typed_list.R
+++ tests/testthat/test-typed_list.R
@@ -29,3 +29,18 @@ test_that("when pass list with one named list item expect successful coersion to
 test_that("when pass list with multiple items with different classes expect coersion to typed_list fails", {
   expect_error(as.typed_list(list(list(a = 1, b = 2), structure(list(a = 1), class = "non-standard class"))))
 })
+
+test_that("when get typed_list.typeof expect generic type is returned", {
+  expect_match(typed_list.typeof(as.typed_list(list(list(a = 1), list(a = 3), list(b = 4)))),
+               "list")
+  tlist <- as.typed_list(list(structure(list(a = 1), class = "some_class"), structure(list(b = 1), class = "some_class")))
+  expect_match(typed_list.typeof(tlist), "some_class")
+})
+
+test_that("when get names via subset operator expect vector of properties from all items", {
+  expect_equal(as.typed_list(list(list(a = 1), list(a = 2), list(a = 3)))$a,
+               1:3)
+
+  expect_equal(as.typed_list(list(list(a = 1), list(b = 2)))$a,
+               list(1,NULL))
+})
