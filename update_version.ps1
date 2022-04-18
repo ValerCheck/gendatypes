9b1911f47cf7cb79404ec0a6ef9fbd8176040033
ValerCheck <mypowerismind@gmail.com>
2022-04-18 14:23:55 +0300

    Update version: Version: 0.0.0.9

---
+++ update_version.ps1
@@ -0,0 +1,20 @@
+param (
+    [string]$version = $null,
+    [switch]$autoIncrement = $true
+)
+
+Function Get-AutoIncrementedVersion {
+    Param ([string]$CurrentVersionString)
+
+    $currentVersion = [System.Version]::Parse($CurrentVersionString.Split(" ")[1])
+    $newVersionString = "{0}.{1}.{2}.{3}" -f ($currentVersion.Major, $currentVersion.Minor, $currentVersion.Build, $($currentVersion.Revision + 1))
+    "Version: {0}" -f $newVersionString
+}
+
+$currentVersionString = @(cat "DESCRIPTION") -match "Version"
+Write-Host $currentVersionString
+$newVersionString = &{if ($autoIncrement -eq $true) {Get-AutoIncrementedVersion -CurrentVersionString $currentVersionString} else {$version} }
+((Get-Content -Path "DESCRIPTION" -Raw) -replace $currentVersionString, $newVersionString) | Set-Content -Path "DESCRIPTION"
+
+git add .
+git commit -m ("Update version: {0}" -f $newVersionString)
