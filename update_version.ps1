39ab78f8a6e2238e29d39d6c5f150b121e79702c
ValerCheck <mypowerismind@gmail.com>
2022-04-18 14:56:14 +0300

    Update update script to be more quiet on git push

--- update_version.ps1
+++ update_version.ps1
@@ -21,5 +21,5 @@ $newVersion = ("v{0}" -f ($newVersionString | Select-String -Pattern "\d.+").Mat
 git add .
 git commit -m ("Update version: {0}" -f $newVersion)
 git tag $newVersion
-git push
-git push --tags
+git push --quiet
+git push --tags --quiet
